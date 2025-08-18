a0e3b37d6c5c19d2ccc41a08534aa3b54078da93
obonyodorice <obonyodorice3@gmail.com>
2025-08-18 16:46:08 -0700

    cm

--- forums/admin.py
+++ forums/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Topic, Post
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'is_pinned', 'is_locked')
+    list_filter = ('category', 'is_pinned', 'is_locked')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'author', 'created_at')
+    list_filter = ('topic__category', 'created_at')
