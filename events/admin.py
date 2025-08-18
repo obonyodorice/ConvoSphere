30a73a9d56a3f134baa68adf40b277caff43d3d8
obonyodorice <obonyodorice3@gmail.com>
2025-08-17 17:08:19 -0700

    cm

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event, EventAgenda
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_type', 'start_time', 'created_by')
+    list_filter = ('event_type', 'start_time', 'is_public')
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('speakers', 'attendees')
+
+@admin.register(EventAgenda)
+class EventAgendaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event', 'start_time', 'speaker')
+    list_filter = ('event', 'start_time')
