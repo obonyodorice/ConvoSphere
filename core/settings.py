cf3a72277d0e84006a7b87c5cf2c4f9946cdbad4
obonyodorice <obonyodorice3@gmail.com>
2025-08-23 13:50:25 -0700

    cm

--- core/settings.py
+++ core/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# for production (collects all static files into one folder)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
