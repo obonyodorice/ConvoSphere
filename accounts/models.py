64f5ebaf4f829248d8db9add7f964fb9a92c7fc8
obonyodorice <obonyodorice3@gmail.com>
2025-08-25 20:24:05 -0700

    cm

--- accounts/models.py
+++ accounts/models.py
@@ -21,3 +21,15 @@ class User(AbstractUser):
     
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['is_online', 'last_seen']),
+        ]   
+    
+    @property
+    def recently_seen(self):
+        """True if user was active in last 5 minutes (300 seconds)."""
+        if not self.last_seen:
+            return False
+        return (timezone.now() - self.last_seen).total_seconds() < 300
