77bee19bedd7ba0d864826f703cd95cfd820be37
obonyodorice <obonyodorice3@gmail.com>
2025-08-26 21:26:48 -0700

    cm

---
+++ chat/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/room-list/$', consumers.RoomListConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_id>[0-9a-f-]+)/$', consumers.ChatRoomConsumer.as_asgi()),
+]
+
