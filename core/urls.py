24a3ef8da3809757a0dd3de4b718cb48ce2dad81
obonyodorice <obonyodorice3@gmail.com>
2025-08-28 20:57:24 -0700

    cm

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('forums/', include('forums.urls')),
+    # path('forums/', include('forums.urls')),
     path('events/', include('events.urls')),
     path('chat/', include('chat.urls')),
     path('notifications/', include('notifications.urls')),
