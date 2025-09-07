b069e2768c918c3f95745f3c4bfb3116370c7129
obonyodorice <obonyodorice3@gmail.com>
2025-09-06 21:25:08 -0700

    cm

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/<uuid:user_id>/', views.profile_view, name='profile'),
     path('status/update/', views.update_status, name='update_status'),
 ]
