73cda1399455c993b3c8c9be01db068942092751
obonyodorice <obonyodorice3@gmail.com>
2025-10-06 13:34:06 -0700

    cm

--- chat/apps.py
+++ chat/apps.py
@@ -1,12 +1,6 @@
-# chat/apps.py
 from django.apps import AppConfig
 
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
-
-    def ready(self):
-        """Import signal handlers when Django is ready"""
-        from .models import ready
-        ready()
