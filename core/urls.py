3260c3b4f76d238590bd0ae30fcbaaf09938e455
obonyodorice <obonyodorice3@gmail.com>
2025-10-08 22:09:50 -0700

    cm

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     # path('forums/', include('forums.urls')),
-    path('events/', include('events.urls')),
+    # path('events/', include('events.urls')),
     path('chat/', include('chat.urls')),
-    path('notifications/', include('notifications.urls')),
-    path('', include('forums.urls')),  # Default to forums
+    # path('notifications/', include('notifications.urls')),
+    # path('', include('forums.urls')),  # Default to forums
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
